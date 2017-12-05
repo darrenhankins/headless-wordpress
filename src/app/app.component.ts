c1b379c5543f55b90ea1f72c74b2fe229688703c
Darren Hankins <hankins.darren@gmail.com>
2017-12-04 20:58:39 -0700

    changes to app.component.html & .ts and app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  posts = [];
+
+  // title = 'app';
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(): void {
+    this.http.get('http://headless-wordpress.dev:8888/wp-json/wp/v2/posts').subscribe(data => {
+      for(let key in data) {
+        if(data.hasOwnProperty(key)){
+          this.posts.push(data[key]);
+        }
+      }
+      console.log(this.posts);
+    })
+  }
+
 }
